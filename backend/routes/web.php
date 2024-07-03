c33186ccc1533b6050a095676d43e7205f6115af
Stefan <stefan.batandjiev@gmail.com>
2024-07-03 17:30:24 +0200

    Added Auth

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/reset-password/{token}', function ($token){
+    return view('auth.password-reset', [
+        'token' => $token
+    ]);
+})->middleware(['guest:'.config('fortify.guard')])
+    ->name('password.reset');
