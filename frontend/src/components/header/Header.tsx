6083cf3dc9e00ba7427c7361a7c9a1abc6f4ce7a
Stefan <stefan.markovski@teaming.inteligenta.io>
2024-07-08 15:56:46 +0200

    Fixed header issues.

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,5 +1,5 @@
 import LanguageSwitcher from "../language-switcher/LanguageSwitcher";
-import {Box, Typography} from "@mui/material";
+import {Box, Grid, Link, Typography} from "@mui/material";
 import Logo from '../../shared/styles/images/logo.png'
 import AI_Icon from '../../shared/styles/icons/ai_icon.png'
 import Car_Icon from '../../shared/styles/icons/car_icon.png'
@@ -12,21 +12,27 @@ export const Header = () => {
     const {t} = useTranslation()
 
     const headerElements: HeaderElementType[] = [
-        {imageSrc: AI_Icon, text: t('SHTOP_AI')},
-        {imageSrc: Car_Icon, text: t('FIND_TRANSPORT')},
-        {imageSrc: Share_Icon, text: t('SHARE_TRANSPORT')}
+        {imageSrc: AI_Icon, text: t('SHTOP_AI'), href: '/shtop-ai'},
+        {imageSrc: Car_Icon, text: t('FIND_TRANSPORT'), href: '/search-route'},
+        {imageSrc: Share_Icon, text: t('SHARE_TRANSPORT'), href: '/share-route'}
     ];
 
-    return <Box id='header-container'>
-        <img src={Logo} className='logo'/>
-        <Box className='header-elements-container'>
-            {headerElements.map((headerElement, index) => {
-               return <Box key={index} className='header-element'>
+    return <Grid container xs={12} id='header-container'>
+        <Grid item xs={12} lg={4}>
+            <Link href='/'>
+                <img src={Logo} className='logo'/>
+            </Link>
+        </Grid>
+        {headerElements.map((headerElement, index) => {
+            return <Grid item container xs={12} lg={2} key={index} className='header-element-container'>
+                <Link href={headerElement.href} className='header-element'>
                     <img src={headerElement.imageSrc} className='icon'/>
-                    <Typography className='header-element-text' variant='h6'>{headerElement.text}</Typography>
-                </Box>
-            })}
+                    <Typography className='header-element-text' variant='h5'>{headerElement.text}</Typography>
+                </Link>
+            </Grid>
+        })}
+        <Grid item container xs={12} lg={2} className='header-element-container'>
             <LanguageSwitcher/>
-        </Box>
-    </Box>
+        </Grid>
+    </Grid>
 }
